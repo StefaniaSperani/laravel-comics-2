461500b2b98956b1581fd2c64ea2faef35d0b708
StefaniaSperani <thedarkcry@hotmail.it>
2022-12-20 17:32:29 +0100

    add comics + show.blades

--- resources/views/Comics/show.blade.php
+++ resources/views/Comics/show.blade.php
@@ -1,5 +1,16 @@
+@extends('layouts.app')
+
 @section('content')
-<section class="container">
+<section class="container text-dark py-3">
     <h1>{{$comic->title}}</h1>
+    <div class="d-flex align-items-center">
+        <img src="{{$comic->thumb }}" alt="">
+        <div class="ps-3">
+            <h3>{{$comic->series}} - {{$comic->price}}</h3>
+            <p>{{$comic->description}}</p>
+        </div>
+
+    </div>
+
 </section>
 @endsection
